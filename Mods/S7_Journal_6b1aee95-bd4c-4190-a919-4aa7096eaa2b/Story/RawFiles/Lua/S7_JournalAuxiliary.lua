b2ece1556b9e7ec3e00edbb90cca2174634d6685
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2020-12-04 10:35:39 +0530

    ValidString Check

--- Mods/S7_Journal_6b1aee95-bd4c-4190-a919-4aa7096eaa2b/Story/RawFiles/Lua/S7_JournalAuxiliary.lua
+++ Mods/S7_Journal_6b1aee95-bd4c-4190-a919-4aa7096eaa2b/Story/RawFiles/Lua/S7_JournalAuxiliary.lua
@@ -1,3 +1,15 @@
 --  IMPORT DEPENDENCY
 
 UCL = Mods["S7_UI_Components_Library"]
+
+--  =================================
+--      VALIDATE NON-EMPTY STRING
+--  =================================
+
+function ValidString(str) --  Checks if a string is not nil and is not empty.
+    if type(str) == "string" and str ~= nil and str ~= "" and str ~= "{}" and str ~= "[]" then
+        return true
+    else
+        return false
+    end
+end
