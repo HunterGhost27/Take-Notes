14f919302e04b0a853448414a835b21e8b90356d
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2020-12-12 22:52:52 +0530

    Added DebugMode ConsoleCommand

--- Mods/S7_Journal_6b1aee95-bd4c-4190-a919-4aa7096eaa2b/Story/RawFiles/Lua/S7_DebugMode.lua
+++ Mods/S7_Journal_6b1aee95-bd4c-4190-a919-4aa7096eaa2b/Story/RawFiles/Lua/S7_DebugMode.lua
@@ -2,8 +2,14 @@
 --  ADD ITEM TO HOST CHARACTER
 --  ==========================
 
-Ext.RegisterOsirisListener("SavegameLoaded", 4, "after", function ()
-    if Osi.ItemTemplateIsInCharacterInventory(Osi.CharacterGetHostCharacter(), JournalTemplate) < 1 then
-        Osi.ItemTemplateAddTo(JournalTemplate, Osi.CharacterGetHostCharacter(), 1, 1)
-    end
+local function addJournal()
+    Osi.ItemTemplateAddTo(JournalTemplate, Osi.CharacterGetHostCharacter(), 1, 1)
+end
+
+Ext.RegisterConsoleCommand("S7_Journal", function (cmd, command)
+    if command == "AddJournal" then addJournal() end
+end)
+
+Ext.RegisterOsirisListener("GameStarted", 2, "after", function (level, isEditorMode)
+    if Osi.ItemTemplateIsInCharacterInventory(Osi.CharacterGetHostCharacter(), JournalTemplate) < 1 then addJournal() end
 end)
