2b749898ee39e21bcae0b8b32218b82fbba5607e
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2020-12-24 01:30:28 +0530

    Changed UCL.Build to take table input instead

--- Mods/S7_Journal_6b1aee95-bd4c-4190-a919-4aa7096eaa2b/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/S7_Journal_6b1aee95-bd4c-4190-a919-4aa7096eaa2b/Story/RawFiles/Lua/BootstrapClient.lua
@@ -33,12 +33,12 @@ Ext.RegisterNetListener(IDENTIFIER, function (channel, payload)
         S7DebugPrint("Dispatching BuildSpecs to UI-Components-Library.", "BootstrapClient")
         local BuildSpecs = {["GMJournal"] = Rematerialize(journal.Data.content)}
         if not UCL.Journal.Exists then
-            UCL.UCLBuild(Ext.JsonStringify(BuildSpecs))
+            UCL.UCLBuild(BuildSpecs)
             Ext.RegisterUICall(UCL.Journal.UI, "S7_Journal_UI_Hide", function (ui, call, ...)
                 SaveJournal()
                 UCL.UnloadJournal()
             end)
-        else UCL.UCLBuild(Ext.JsonStringify(BuildSpecs)) end
+        else UCL.UCLBuild(BuildSpecs) end
         SaveJournal()
     end
 end)
